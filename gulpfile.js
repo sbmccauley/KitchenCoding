87175586fdc2514e441cba1a400d9ca630779d14
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2020-03-04 22:48:09 -0800

    worked on about page, edited images, minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var {src,dest, watch} = require('gulp');
 var sass = require('gulp-sass');
+var clean = require('gulp-clean-css');
 sass.compiler = require('node-sass');
 
 function css () {
@@ -8,8 +9,17 @@ function css () {
     .pipe(dest('dist/css'))
 };
 
+function min () {
+    return src('src/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(dest('dist/css'))
+};
+
 //exports.default = css;
 
 exports.default = function () {
     watch('src/*.scss', css);
+    watch('src/modules/*.scss', css);
+   
+
 }
